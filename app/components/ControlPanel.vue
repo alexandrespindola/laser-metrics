b5741455e086882c045c42a939bd0945e9a51006
Alexandre Spindola <alexandre.spindola11@gmail.com>
2025-11-18 16:22:33 +0100

    Remove LaserMetrics component and replace with ControlPanel in index page
    Deletes the LaserMetrics.vue component file and updates the index page to use ControlPanel component instead. Removes the start script from package.json that referenced the deleted dev.sh file.

---
+++ app/components/ControlPanel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card bg-base-200 p-4 shadow-lg">
+    <div class="flex flex-wrap gap-4 justify-center">
+      <button @click="startMonitoring" class="btn btn-primary" :disable="isMonitoring">
+        Start Monitoring
+      </button>
+      <button @click="stopMonitoring" class="btn btn-error" :disable="!isMonitoring">
+        Stop Monitoring
+      </button>
+      <button @click="showStatistics" class="btn btn-secondary" :disable="!hasData">
+        Show Statistics
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import { ref } from 'vue'
+
+const isMonitoring = ref(false)
+const hasData = ref(false)
+
+const emit = defineEmits(['start', 'stop', 'show-stats'])
+
+const startMonitoring = () => {
+  isMonitoring.value = true
+  hasData.value = true
+  emit('start')
+}
+
+const stopMonitoring = () => {
+  isMonitoring.value = false
+  emit('stop')
+}
+
+const showStatistics = () => {
+  emit('show-stats')
+}
+
+</script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <LaserMetrics />
+        <ControlPanel />
     </div>
 </template>
